81042d094f630f60a41a2c236793e3cebab3093b
amirtechnoide <amirzoutor@gmail.com>
2020-12-01 14:47:16 +0100

    delete everything to start a fresh project

--- grid.js
+++
@@ -1,7 +0,0 @@
-const GRID_SIZE = 21
-export function randomGridPosition() {
-    return {
-        x: Math.floor(Math.random() * GRID_SIZE ) + 1,
-        y: Math.floor(Math.random() * GRID_SIZE ) + 1
-    }
-}
